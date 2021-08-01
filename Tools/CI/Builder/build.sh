f230434749dc740066668d608129c1302385eabf
Dinesh B Panchal <panchaldineshb@gmail.com>
2021-07-31 22:58:09 -0400

    DP:Changed yaml---17

--- Tools/CI/Builder/build.sh
+++ Tools/CI/Builder/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Hello World --- build
-apt update && sudo apt upgrade -y
-apt install curl -y
+sudo -s apt update && sudo -s apt upgrade -y
+sudo -s apt install curl -y
 curl --version
 curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.0.30.zip -o awscliv2.zip
 unzip awscliv2.zip
